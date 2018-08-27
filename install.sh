25d7593e114fff2a2a30c68455a6fd2bbcd39997
goga <goga.mamoulasvili@gmail.com>
2018-08-27 14:18:12 +0300

    Install all file

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Download and install sbopkg 
+cd $PWD/sbopkg
+sh sbopkg.sh
+
+# Download install nvidia bumblebee (primus)
+cd $PWD/primus
+sh primus.sh
+
+# Install multilib packages
+cd $PWD/multilib
+sh multilib.sh
+
+# Install i3 window manager
+cd $PWD
+sh pkg-install i3
+
+# Install urxvt terminal
+cd $PWD
+sh pkg-install rxvt-unicode
+
