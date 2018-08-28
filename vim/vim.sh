4e9bfd4912b8a6496d7e90f022414352df8aeccc
goga <goga.mamoulasvili@gmail.com>
2018-08-28 17:36:14 +0300

    Compile vim with custom parameters (python, clipboard support)

--- vim/vim.sh
+++ vim/vim.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+# Download & install ack executable
+curl https://beyondgrep.com/ack-2.24-single-file > /usr/bin/ack && chmod 0755 /usr/bin/ack 
+
 # Install vim
 echo "Installing latest vim"
-git clone https://github.com/vim/vim.git
+# git clone https://github.com/vim/vim.git
 cd vim/src
+./configure --with-features=huge \
+            --enable-multibyte \
+	    --enable-rubyinterp=yes \
+	    --enable-pythoninterp=yes \
+	    --with-python-config-dir=/usr/lib64/python2.7/config \ # pay attention here check directory correct
+	    --enable-python3interp=yes \
+	    --with-python3-config-dir=/usr/lib64/python.3.6/config \
+	    --enable-perlinterp=yes \
+	    --enable-luainterp=yes \
+            --enable-gui=gtk2 \
+            --enable-cscope \
+	   --prefix=/usr/local
+
 make
+make install
 
 # Override existing /usr/bin/vim for admin
 cp /usr/local/bin/vim /usr/bin/vim
