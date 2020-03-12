8c24c9f55cd735a5df8a8cb7988a45a9f4b8dc31
goga-m <goga.mamoulasvili@gmail.com>
2020-03-12 09:21:26 +0200

    Use install.py on Ycm installation

--- vim/vim-youcompleteme.sh
+++ vim/vim-youcompleteme.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Compile Youcomplete me
-# See more https://valloric.github.io/YouCompleteMe/#full-installation-guide
+# See more http://ycm-core.github.io/YouCompleteMe/#full-installation-guide
 mkdir ~/.vim/bundle/YouCompleteMe/ycm_build
 cd ~/.vim/bundle/YouCompleteMe/ycm_build
 
 cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/
 
-cd CMakeFiles
-cmake --build . --target ycm_core --config Release
+cd ~/.vim/bundle/YouCompleteMe/
+sh install.py
+# cd CMakeFiles
+# cmake --build . --target ycm_core --config Release
