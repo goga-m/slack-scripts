0ab8d2d289e1b6c6349172783ed267e524530dde
goga-m <goga.mamoulasvili@gmail.com>
2020-05-13 16:54:12 +0300

    Add installation script for vim-plug

---
+++ vim/vim-plug.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install vim-plug
+# See: https://github.com/junegunn/vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
